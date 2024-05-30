35b69d462456726ef8b48dbf6f180fb37ff45656
Михайло Капітанов <Mykhailo.Kapitanov@infiz.khpi.edu.ua>
2024-05-30 22:39:11 +0300

    Добавьте файлы проекта.

---
+++ Lab19_kapitanov_var10/Lab19_kapitanov_var10.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+template <typename T1, typename T2, typename T3>
+auto geometricMean(T1 a, T2 b, T3 c) {
+    return pow(a * b * c, 1.0 / 3);
+}
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+    cout << "Введiть три цiлих додатнiх числа" << endl;
+    int a, b, c;
+    cout << "Перше число: ";
+    cin >> a;
+    cout << "Друге число: ";
+    cin >> b;
+    cout << "Третє число: ";
+    cin >> c;
+    auto result = geometricMean(a, b, c);
+    cout << "Середнє геометричне чисел " << a << ", " << b << ", " << c << " дорiвнює " << result << endl;
+    return 0;
+}
